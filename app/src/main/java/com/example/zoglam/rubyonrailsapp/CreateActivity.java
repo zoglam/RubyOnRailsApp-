f541eb6ca0ad7daff1a205b33bdd889b08b909d8
zoglam <anzoor1998@gmail.com>
2019-08-13 19:02:07 +0300

    Полный переход на новой формат приложения

--- app/src/main/java/com/example/zoglam/rubyonrailsapp/CreateActivity.java
+++ app/src/main/java/com/example/zoglam/rubyonrailsapp/CreateActivity.java
@@ -1,87 +1,89 @@
 package com.example.zoglam.rubyonrailsapp;
 
 import android.content.Context;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.WindowManager;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
+import android.widget.CheckedTextView;
+import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.TextView;
-
-import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+import com.google.gson.JsonObject;
+import com.koushikdutta.ion.Ion;
+import java.util.ArrayList;
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
-
 public class CreateActivity extends AppCompatActivity {
-    private int checkedProject = -1;
-    private TextView tempTv;
+
+    private ListView projectsList;
+    private EditText value;
+    private RelativeLayout interceptor;
+    static CheckedTextView checkedTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.createtodo_toolbar);
 
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.activity_create);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
 
-
-
-        Toolbar tb = (Toolbar)findViewById(R.id.toolbar_creation);
-        tb.setNavigationIcon(R.drawable.backlcon);
-        setSupportActionBar(tb);
-        tb.setNavigationOnClickListener(new View.OnClickListener() {
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
             }
         });
 
+        interceptor = (RelativeLayout) findViewById(R.id.createtodo_content);
+        value= (EditText) findViewById(R.id.EditText);
 
-
-        final String[] projectsArray = getIntent().getExtras().getStringArray("projects");
-        final ListView projectsList = (ListView) findViewById(R.id.list);
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.projectlist_cell);
-        adapter.addAll(projectsArray);
+        Bundle b = this.getIntent().getExtras();
+        ArrayList<String> projectsArray = b.getStringArrayList("projectsArray");
+        projectsList = (ListView) findViewById(R.id.projectsList);
+        ProjectAdapter adapter = new ProjectAdapter(this, projectsArray);
         projectsList.setAdapter(adapter);
-
-        projectsList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                if(position != checkedProject && checkedProject >= 0)
-                    tempTv.setCompoundDrawablesWithIntrinsicBounds(0, 0, 0, 0);
-
-                tempTv = ((TextView) view);
-                tempTv.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.done, 0);
-                checkedProject = position;
-            }
-        });
     }
 
     @Override
-    protected void attachBaseContext(Context newBase) {
-        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu){
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.my_menu, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()){
-            case R.id.action_settings:
-                finish();
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
+        switch (item.getItemId())
+        {
+            case R.id.save:
+                value = (EditText)findViewById(R.id.EditText);
+                if (value.getText().length() > 3 && checkedTextView != null) {
+                    JsonObject param = new JsonObject();
+                    param.addProperty("text", value.getText().toString());
+                    param.addProperty("project_id", checkedTextView.getId());
+                    JsonObject params = new JsonObject();
+                    params.add("newTodo", param);
+                    Ion.with(getApplicationContext())
+                            .load("POST", "http://95.165.148.222:3000/project")
+                            .setJsonObjectBody(params).asJsonObject();
+                    Toast.makeText(getApplicationContext(), getString(R.string.alarmGOOD), Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+                else Toast.makeText(getApplicationContext(), getString(R.string.alarmBAD), Toast.LENGTH_SHORT).show();
         }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
     }
 }
--- app/src/main/java/com/example/zoglam/rubyonrailsapp/CustomAdapter.java
+++ app/src/main/java/com/example/zoglam/rubyonrailsapp/CustomAdapter.java
@@ -8,64 +8,36 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
-import android.widget.TextView;
-import android.widget.CompoundButton;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.TextView;
 
-import static android.R.attr.checked;
+import static com.example.zoglam.rubyonrailsapp.MainActivity.todos;
 
 class CustomAdapter extends BaseAdapter {
 
     private static final int TYPE_ITEM = 0;
     private static final int TYPE_SEPARATOR = 1;
 
-    private static final int STRIKE_LINE = Paint.STRIKE_THRU_TEXT_FLAG;
-
     private ArrayList<String> mData = new ArrayList<String>();
     private TreeSet<Integer> sectionHeader = new TreeSet<Integer>();
 
-    private boolean[] checkOfBooleanVar;
-    private int counter = 0;
-
     private LayoutInflater mInflater;
 
-    CustomAdapter(Context context) {
+    public CustomAdapter(Context context) {
         mInflater = (LayoutInflater) context
                 .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
     }
 
-    boolean[] itemCount(int count){
-        return checkOfBooleanVar = new boolean[count];
-    }
-
-    void addItem(final String item, boolean isCompleted, int index) {
-        checkOfBooleanVar[index] = isCompleted;
+    public void addItem(final String item) {
         mData.add(item);
         notifyDataSetChanged();
     }
 
-    void addSectionHeaderItem(final String item) {
+    public void addSectionHeaderItem(final String item) {
         mData.add(item);
         sectionHeader.add(mData.size() - 1);
         notifyDataSetChanged();
-    }
-
-    private void lineStriking(CompoundButton view, boolean isChecked){
-        if(isChecked){
-            view.setPaintFlags(view.getPaintFlags() | STRIKE_LINE);
-        }
-        else{
-            view.setPaintFlags(view.getPaintFlags() ^ STRIKE_LINE);
-        }
-    }
-
-    private void lineStriking(ViewHolder vh, boolean ic){
-        if(ic){
-            vh.checkBox.setPaintFlags(vh.checkBox.getPaintFlags() | STRIKE_LINE);
-        }
-        else{
-            vh.checkBox.setPaintFlags(vh.checkBox.getPaintFlags() & STRIKE_LINE);
-        }
     }
 
     @Override
@@ -95,37 +67,52 @@ class CustomAdapter extends BaseAdapter {
 
     public View getView(int position, View convertView, ViewGroup parent) {
         ViewHolder holder = null;
-
         int rowType = getItemViewType(position);
 
         if (convertView == null) {
             holder = new ViewHolder();
             switch (rowType) {
                 case TYPE_ITEM:
-                    convertView = mInflater.inflate(R.layout.snippet_item1, null);
-                    holder.checkBox = (CheckBox) convertView.findViewById(R.id.todo_cb);
-                    holder.checkBox.setText(mData.get(position));
-                    holder.checkBox.setChecked(checkOfBooleanVar[counter++]);
-                    lineStriking(holder, holder.checkBox.isChecked());
-                    holder.checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-                        public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-
-                            Todo todoInFocus = (Todo) buttonView.getTag();
-                            //if (totoInFocus.isCompleted == isChecked) return;
-                            lineStriking(buttonView, isChecked);
-                        }
-                    });
-
+                    convertView = mInflater.inflate(R.layout.activity_checkbox, null);
+                    holder.checkBox = (CheckBox) convertView.findViewById(R.id.checkBox);
                     break;
                 case TYPE_SEPARATOR:
-                    convertView = mInflater.inflate(R.layout.snippet_item2, null);
+                    convertView = mInflater.inflate(R.layout.activity_projectname, null);
                     holder.textView = (TextView) convertView.findViewById(R.id.textSeparator);
-                    holder.textView.setText(mData.get(position));
                     break;
             }
             convertView.setTag(holder);
         } else {
             holder = (ViewHolder) convertView.getTag();
+        }
+        if (holder.textView != null) holder.textView.setText(mData.get(position));
+        for (int i = 0; i < todos.size(); i++) {
+            MainActivity.Todo todo = todos.get(i);
+            if (mData.get(position).equals(todo.text)) {
+                holder.checkBox.setText(todo.text);
+                holder.checkBox.setId(i);
+                holder.checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+                        MainActivity.Todo todoIsCompl = todos.get(buttonView.getId());
+
+                        if ( todoIsCompl.isCompleted == isChecked) return;
+                        MainActivity.update(todoIsCompl.todo_id);
+                        if ( todoIsCompl.isCompleted) {
+                            todoIsCompl.isCompleted = false;
+                            buttonView.setPaintFlags(buttonView.getPaintFlags() & (~ Paint.STRIKE_THRU_TEXT_FLAG));
+                        }
+                        else {
+                            todoIsCompl.isCompleted = true;
+                            buttonView.setPaintFlags(buttonView.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
+                        }
+
+                    }
+                });
+                holder.checkBox.setChecked(todo.isCompleted);
+                if (holder.checkBox.isChecked()) holder.checkBox.setPaintFlags(holder.checkBox.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
+                else holder.checkBox.setPaintFlags(holder.checkBox.getPaintFlags() & (~ Paint.STRIKE_THRU_TEXT_FLAG));
+            }
         }
 
         return convertView;
@@ -133,7 +120,7 @@ class CustomAdapter extends BaseAdapter {
 
     public static class ViewHolder {
         public TextView textView;
-        CheckBox checkBox;
+        public CheckBox checkBox;
     }
 
 }
